235c3ff702455db08d088437e819d72a47749019
U2FsdGVkX1 <U2FsdGVkX1@gmail.com>
2016-12-10 22:36:26 +0800

    更换代码编辑器

--- Templates/PluginMain.php
+++ Templates/PluginMain.php
@@ -1,5 +1,6 @@
 <?php
     $code = file_get_contents (APP_PATH . '/Plugins/' . $this->param[0] . '/' . $this->param[0] . '.class.php');
+    $code = htmlspecialchars ($code);
 ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
@@ -7,8 +8,12 @@
         <title>设置</title>
         <?php require_once 'Header.php' ?>
         <style>
-            .code {
+            #code {
+                position: absolute;
+                top: 0px;
+                left: 0px;
                 width: 100%;
+                height: 750px;
             }
         </style>
     </head>
@@ -17,15 +22,22 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-12" style="margin-top: 10px">
-                    <textarea id="code" class="code" rows="50"><?php echo $code ?></textarea>
+                    <div id="code"><?php echo $code ?></div>
                 </div>
-                <div class="col-xs-12">
+                <div class="col-xs-12" style="margin-top: 760px">
                     <button id="save" style="float: right" type="button" class="btn btn-success">编辑</button>
                 </div>
             </div>
         </div>
         <?php require_once 'Footer.php' ?>
+        <script src="<?php echo $this->loadSource ("assets/ace/ace.js") ?>" type="text/javascript" charset="utf-8"></script>
         <script>
+            var editor = ace.edit("code");
+            editor.getSession().setMode("ace/mode/php");
+            editor.setTheme("ace/theme/clouds");
+            editor.setFontSize(16);
+            editor.setShowPrintMargin(false);
+            
             $("button#save").click(function(){
                 buttonThis = $(this);
                 $(buttonThis).attr("disabled", "disabled");
@@ -34,7 +46,7 @@
                     url: "<?php echo APP_URL ?>/index.php/PluginMain/ajaxSave",
                     data: {
                         "pcn": "<?php echo $this->param[0] ?>",
-                        "code": $("textarea#code").val()
+                        "code": editor.getValue()
                     },
                     success: function(data, textStatus, jqXHR){
                         if(data.code == '0'){
