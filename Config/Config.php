388ceefd91b8c5fd689747d6c4348e136c1e951a
U2FsdGVkX1 <U2FsdGVkX1@gmail.com>
2016-12-25 08:57:31 +0800

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,6 +1,6 @@
 <?php
-    define ('DBHOST', '');
-    define ('DBNAME', '');
-    define ('DBUSER', '');
-    define ('DBPASS', '');
+    define ('DBHOST', '这里填写数据库地址');
+    define ('DBNAME', '这里填写数据库名');
+    define ('DBUSER', '这里填写数据库用户名');
+    define ('DBPASS', '这里填写数据库密码');
     define ('DBPERSISTENT', false);
