03d69facb3c891ab7eca8fd0081213fa413591b4
U2FsdGVkX1 <U2FsdGVkX1@gmail.com>
2019-05-01 12:07:03 +0800

    改为 medoo 驱动

--- Plugins/Backup/Backup.class.php
+++ Plugins/Backup/Backup.class.php
@@ -11,11 +11,11 @@
 class Backup extends Base {
     public function command ($command, $param, $message_id, $from, $chat, $date) {
         if ($command == '/backup') {
-            if(MASTER == '') {
+            if($this->telegram->getMaster() == '') {
                 $this->telegram->sendMessage($chat['id'], "未定义『主人 ID』", $message_id);
                 return;
             }
-            if($chat['id'] != MASTER){
+            if($chat['id'] != $this->telegram->getMaster()){
                 $this->telegram->sendMessage($chat['id'], "你没有权限进行该操作！", $message_id);
                 return;
             }
@@ -25,7 +25,7 @@ class Backup extends Base {
             ini_set("max_execution_time", "23333");//避免数据量过大，导出不全的情况出现。
 
             $msg1 = $this->telegram->sendMessage($chat['id'], '备份马上开始，请稍等', $message_id);
-            $backupFile = $this->EXPORT_TABLES(DBHOST, DBUSER, DBPASS, DBNAME);
+            $backupFile = $this->EXPORT_TABLES(__DIR__ . '/');
             $msg2 = $this->telegram->sendMessage($chat['id'], '备份完成，正在发送文件', $message_id);
             $fileCurl = new CURLFile($backupFile);
             $this->telegram->sendDocument($chat['id'], $fileCurl, '#Backup ' . date('Y-m-d_H-i-s'), $message_id);
@@ -37,34 +37,27 @@ class Backup extends Base {
 
     // https://stackoverflow.com/a/35393866
     // Author: MStanley https://stackoverflow.com/users/5356839/mstanley
-    function EXPORT_TABLES($host, $user, $pass, $name, $tables = false)
+    // Last Modified: U2FsdGVkX1
+    function EXPORT_TABLES($savePath, $tables = false)
     {
-        $savePath = dirname(__FILE__) . "/";
-
-        $mysqli      = new mysqli($host, $user, $pass, $name);
-        $mysqli->select_db($name);
-        $mysqli->query("SET NAMES 'utf8'");
-        $queryTables = $mysqli->query('SHOW TABLES');
-        while ($row         = $queryTables->fetch_row())
-        {
-            $target_tables[] = $row[0];
-        }
+        $this->db->query('SET SQL_MODE = NO_AUTO_VALUE_ON_ZERO');
+        $target_tables = $this->db->query('SHOW TABLES')->fetchAll(PDO::FETCH_COLUMN);
         if ($tables !== false)
         {
             $target_tables = array_intersect($target_tables, $tables);
         }
-        $content = "SET SQL_MODE = \"NO_AUTO_VALUE_ON_ZERO\";\r\nSET time_zone = \"+00:00\";\r\n\r\n\r\n/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;\r\n/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;\r\n/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;\r\n/*!40101 SET NAMES utf8 */;\r\n--Database: `" . $name . "`\r\n\r\n\r\n";
+        $content = "SET SQL_MODE = \"NO_AUTO_VALUE_ON_ZERO\";\r\nSET time_zone = \"+00:00\";\r\n\r\n\r\n/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;\r\n/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;\r\n/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;\r\n/*!40101 SET NAMES utf8 */;\r\n-- Database: `" . DBNAME . "`\r\n\r\n\r\n";
         foreach ($target_tables as $table)
         {
-            $result        = $mysqli->query('SELECT * FROM ' . $table);
-            $fields_amount = $result->field_count;
-            $rows_num      = $mysqli->affected_rows;
-            $res           = $mysqli->query('SHOW CREATE TABLE ' . $table);
-            $TableMLine    = $res->fetch_row();
+            $result        = $this->db->query('SELECT * FROM ' . $table);
+            $fields_amount = $result->columnCount();
+            $rows_num      = $result->rowCount();
+            $res           = $this->db->query('SHOW CREATE TABLE ' . $table);
+            $TableMLine    = $res->fetch();
             $content .= "\n\n" . $TableMLine[1] . ";\n\n";
             for ($i = 0, $st_counter = 0; $i < $fields_amount; $i++, $st_counter = 0)
             {
-                while ($row = $result->fetch_row())
+                while ($row = $result->fetch())
                 { //when started (and every after 100 command cycle):
                     if ($st_counter % 100 == 0 || $st_counter == 0)
                     {
@@ -99,8 +92,9 @@ class Backup extends Base {
                 }
             } $content .="\n\n\n";
         }
+        $this->db->query('SET SQL_MODE = ANSI_QUOTES');
         $content .= "\r\n\r\n/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;\r\n/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;\r\n/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;";
-        $backup_name = $savePath . $name . "_(" . date('Y-m-d_H-i-s') . ")_rand" . rand(1, 11111111) . ".sql.gz";
+        $backup_name = $savePath . DBNAME . "_(" . date('Y-m-d_H-i-s') . ")_rand" . rand(1, 11111111) . ".sql.gz";
 
         file_put_contents($backup_name, gzencode($content, 9));
         return $backup_name;
