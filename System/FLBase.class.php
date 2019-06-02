230fa3ee7c6234e108c6456f9f037648316b0282
somethingNaive1 <51284003+somethingNaive1@users.noreply.github.com>
2019-06-02 15:28:23 +0800

    快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,

--- System/FLBase.class.php
+++ System/FLBase.class.php
@@ -1,17 +1,1 @@
-<?php
-    /**
-     * 真·基类
-     */
-    class FLBase {
-        /**
-         * 加载一个外部库
-         *
-         * @param string  $name 文件名
-         * @param boolean $once 是否检测重复加载
-         * @return void
-        */
-        public function loadLib ($name, $once = true) {
-            $file = LIB_PATH . '/' . $name;
-            $once ? require_once ($file) : require ($file);
-        }
-    }
+快关了你的github别丢人现眼快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,快关了你的github别丢人现眼,,,
